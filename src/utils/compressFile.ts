9d23a990d6ed70b60f958a443dd0f5acf8e3bfa6
Kunal Rajesh Salunkhe <salunkhekunal594@gmail.com>
2024-01-13 01:52:33 +0530

    Add image rotation based on orientation

--- src/utils/compressFile.ts
+++ src/utils/compressFile.ts
@@ -10,8 +10,12 @@ export const compressFile = async (image: Express.Multer.File) => {
     Date.now().toString() + image.originalname
   );
 
+  const { orientation } = await sharp(image.buffer).metadata();
+  const rotate = orientation && orientation >= 5 ? 90 : 0;
+
   const compressedFile = await sharp(image.buffer)
     .webp({ quality: 70 })
+    .rotate(rotate)
     .toFile(compressedImagePath);
 
   return { compressedFile, compressedImagePath };
